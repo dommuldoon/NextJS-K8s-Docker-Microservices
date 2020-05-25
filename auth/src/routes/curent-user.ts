3d09f1bd32c22366be37274e9dcfc5af2dc1c764
Dom Muldoon <dommuldoon@gmail.com>
2020-05-25 20:56:43 +0100

    Auth section

--- auth/src/routes/curent-user.ts
+++ auth/src/routes/curent-user.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { currentUser } from '../middlewares/current-user';
+//import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('api/users/currentuser', (req, res) => {
-	res.send('This is the current user');
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+	res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
